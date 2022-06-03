5b2b33fe66aa3fdde3b93db7d1890e401bdc547b
Romain Dreidemy <dreidemyromain@gmail.com>
2022-06-03 19:58:31 +0200

    Connect mysql database through ssh

---
+++ src/classes/MysqlThroughSSH.ts
@@ -0,0 +1,50 @@
+import {Client, ClientChannel, ConnectConfig} from 'ssh2';
+import mysql, {Connection, ConnectionOptions} from 'mysql2';
+import {IMysqlThroughSSHConfig} from "../types/classes/IMysqlThroughSSHConfig";
+
+export class MysqlThroughSSH {
+
+  private static connect(config: IMysqlThroughSSHConfig): Promise<Connection> {
+    const sshClient = new Client();
+
+    return new Promise((resolve, reject) => {
+      sshClient.on('ready', () => {
+        sshClient.forwardOut(
+          '127.0.0.1',
+          config.database_port,
+          config.ssh_host,
+          config.database_port,
+          (err, stream) => {
+            if (err) {
+              reject(err);
+            }
+
+            const connection = mysql.createConnection(this.getMysqlConnectionOptions(config, stream));
+
+            resolve(connection);
+          });
+      }).connect(this.getSshConnectConfig(config));
+    })}
+
+  private static getMysqlConnectionOptions(config: IMysqlThroughSSHConfig, stream: ClientChannel): ConnectionOptions {
+    return {
+      ...{
+        host: config.ssh_host,
+        port: config.database_port,
+        user: config.database_username,
+        password: config.database_password,
+        database: config.database_name
+      },
+      stream
+    }
+  }
+
+  private static getSshConnectConfig(config: IMysqlThroughSSHConfig): ConnectConfig {
+    return {
+      host: config.ssh_host,
+      port: config.ssh_port,
+      username: config.ssh_username,
+      privateKey: config.ssh_privateKey
+    }
+  }
+}
