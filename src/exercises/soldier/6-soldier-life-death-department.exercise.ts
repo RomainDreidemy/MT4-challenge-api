441f90f0c7e9725c5dd1884fe5901294079a75fe
Romain Dreidemy <dreidemyromain@gmail.com>
2022-06-23 17:38:00 +0200

    check soldier life department

--- src/exercises/soldier/6-soldier-life-death-department.exercise.ts
+++ src/exercises/soldier/6-soldier-life-death-department.exercise.ts
@@ -1,13 +1,18 @@
 import {IMysqlThroughSSHConfig} from "../../types/classes/IMysqlThroughSSHConfig";
 import {MysqlThroughSSH} from "../../classes/MysqlThroughSSH";
+import {ISoldierDepartmentCount} from "../../types/challenges/exercises/ISoldierDepartmentCount";
 
 const EXPECTED_DEATH_DEPARTMENT = 17;
-const EXPECTED_LIFE_DEPARTMENT = 5;
+const EXPECTED_LIFE_DEPARTMENT = 30;
 
 const soldierLifeDeathDepartment = async (config: IMysqlThroughSSHConfig) => {
   await checkSoldierDeathDepartmentCount(config);
 
-  await checkDeathDepartementIdsInSoldierTable(config);
+  await checkDeathDepartmentIdsInSoldierTable(config);
+
+  await checkSoldierLifeDepartmentCount(config);
+
+  await checkLifeDepartmentIdsInSoldierTable(config);
 }
 
 const checkSoldierDeathDepartmentCount = async (config: IMysqlThroughSSHConfig) => {
@@ -19,13 +24,8 @@ const checkSoldierDeathDepartmentCount = async (config: IMysqlThroughSSHConfig) 
   }
 }
 
-const checkDeathDepartementIdsInSoldierTable = async (config: IMysqlThroughSSHConfig) => {
-  interface SoldierDepartmentCount {
-    value: string,
-    expectedCount: number
-  }
-
-  const departments: SoldierDepartmentCount[] = [
+const checkDeathDepartmentIdsInSoldierTable = async (config: IMysqlThroughSSHConfig) => {
+  const departments: ISoldierDepartmentCount[] = [
     { value: 'Allemagne', expectedCount: 20506 },
     { value: 'Allier', expectedCount: 2555 },
     { value: 'Alpes Maritimes', expectedCount: 35932 },
@@ -35,8 +35,34 @@ const checkDeathDepartementIdsInSoldierTable = async (config: IMysqlThroughSSHCo
     { value: 'Calvavdos', expectedCount: 2530 },
   ];
 
+  await testCountDepartments('soldier_death_department','death_department_id', departments, config);
+}
+
+const checkSoldierLifeDepartmentCount = async (config: IMysqlThroughSSHConfig) => {
+  const response = await MysqlThroughSSH.query('select count(*) as nb_department from soldier_life_after_department', config);
+  const nb_department = response[0].nb_department;
+
+  if (nb_department !== EXPECTED_LIFE_DEPARTMENT) {
+    throw new Error('le nombre de département dans la table \'soldier_life_after_department\' ne correspond pas avec ce qui est attendu.')
+  }
+}
+
+const checkLifeDepartmentIdsInSoldierTable = async (config: IMysqlThroughSSHConfig) => {
+  const departments: ISoldierDepartmentCount[] = [
+      { value: 'Alpes Maritimes', expectedCount: 8321 },
+      { value: 'Ain', expectedCount: 1514 },
+      { value: 'Basse Pyrénées', expectedCount: 728 },
+      { value: 'Ile de France', expectedCount: 5247 },
+      { value: 'Var', expectedCount: 28481 },
+      { value: 'Seine et Oise', expectedCount: 769 },
+    ];
+
+  await testCountDepartments('soldier_life_after_department', 'life_after_department_id', departments, config);
+}
+
+const testCountDepartments = async (table: string, relation_column_name: string, departments: ISoldierDepartmentCount[], config:IMysqlThroughSSHConfig) => {
   for (const department of departments) {
-    const query = `select count(*) from soldier_death_department sdd inner join soldier s on sdd.id = s.death_department_id and sdd.name = "${department.value}"`;
+    const query = `select count(*) as nb_department from ${table} sdd inner join soldier s on sdd.id = s.${relation_column_name} and sdd.name = "${department.value}"`;
     const response = await MysqlThroughSSH.query(query, config);
     const nb_department = response[0].nb_department;
 
