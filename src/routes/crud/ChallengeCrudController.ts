3c6c25f96a4ef94a3657f54d9e68759bea566992
Romain Dreidemy <dreidemyromain@gmail.com>
2022-07-03 15:33:45 +0200

    crud challenge read

--- src/routes/crud/ChallengeCrudController.ts
+++ src/routes/crud/ChallengeCrudController.ts
@@ -1,8 +1,11 @@
-import {Get, Route, Security} from 'tsoa';
+import {Get, Path, Route, Security} from 'tsoa';
 import {Crud} from "../../classes/Crud";
 import {IChallenge} from "../../types/tables/challenge/IChallenge";
+import {ApiError} from "../../classes/Errors/ApiError";
+import {ErrorCode} from "../../classes/Errors/ErrorCode";
 
-const READ_COLUMNS = ['id', 'name', 'batch_id'];
+const READ_COLUMNS  = ['id', 'name', 'batch_id'];
+const TABLE_NAME    = 'challenge'
 
 /**
  * Les challenges de la plateforme.
@@ -12,12 +15,26 @@ const READ_COLUMNS = ['id', 'name', 'batch_id'];
 export class ChallengeCrudController {
 
   /**
-   * Test du challenge soldier.
+   * Récupère tous les challenges.
    */
   @Get("/")
   public async index(): Promise<IChallenge[]> {
-    const response = await Crud.Index<IChallenge>({}, 'challenge', READ_COLUMNS);
+    const response = await Crud.Index<IChallenge>({}, TABLE_NAME, READ_COLUMNS);
 
     return response.rows;
+  }
+
+  /**
+   * Récupère un challenge grâce a un identifiant.
+   */
+  @Get("/{id}")
+  public async read(@Path() id: number): Promise<IChallenge> {
+    const response = await Crud.Read<IChallenge>(TABLE_NAME, ['id'], [id], READ_COLUMNS);
+
+    if (response === null) {
+      throw new ApiError(ErrorCode.BadRequest, 'sql/not-found', `Could not found a challenge`);
+    }
+
+    return response;
   }
 }
