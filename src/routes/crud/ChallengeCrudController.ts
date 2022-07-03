0c5b020d314df4422e471c11047edb783af5bcfb
Romain Dreidemy <45939424+RomainDreidemy@users.noreply.github.com>
2022-07-03 16:58:35 +0200

    Feature/crud/challenges (#22)
    * crud challenge index
    
    * fix is_admin scopes
    
    * crud challenge read
    
    * crud challenge create and update
    
    * regroup challenge crud route in swagger
    
    * update swagger documentation

---
+++ src/routes/crud/ChallengeCrudController.ts
@@ -0,0 +1,59 @@
+import {Body, Get, Path, Post, Put, Route, Security, Tags} from 'tsoa';
+import {Crud} from "../../classes/Crud";
+import {IChallenge, IChallengeCreate, IChallengeUpdate} from "../../types/tables/challenge/IChallenge";
+import {ApiError} from "../../classes/Errors/ApiError";
+import {ErrorCode} from "../../classes/Errors/ErrorCode";
+import {ICreateResponse} from "../../types/api/ICreateResponse";
+import {IUpdateResponse} from "../../types/api/IUpdateResponse";
+
+const READ_COLUMNS  = ['id', 'name', 'batch_id'];
+const TABLE_NAME    = 'challenge'
+
+/**
+ * Les challenges de la plateforme.
+ */
+@Route("/challenges")
+@Tags("Challenges")
+@Security('jwt', ['admin'])
+export class ChallengeCrudController {
+
+  /**
+   * Récupère tous les challenges.
+   */
+  @Get("/")
+  public async index(): Promise<IChallenge[]> {
+    const response = await Crud.Index<IChallenge>({}, TABLE_NAME, READ_COLUMNS);
+
+    return response.rows;
+  }
+
+  /**
+   * Récupère un challenge grâce a un identifiant.
+   */
+  @Get("/{id}")
+  public async read(@Path() id: number): Promise<IChallenge> {
+    const response = await Crud.Read<IChallenge>(TABLE_NAME, ['id'], [id], READ_COLUMNS);
+
+    if (response === null) {
+      throw new ApiError(ErrorCode.BadRequest, 'sql/not-found', `Could not found a challenge`);
+    }
+
+    return response;
+  }
+
+  /**
+   * Création d'un challenge.
+   */
+  @Post("/")
+  public async create(@Body() body: IChallengeCreate): Promise<ICreateResponse> {
+    return await Crud.Create<IChallengeCreate>(body, TABLE_NAME);
+  }
+
+  /**
+   * Mise à jour d'un challenge.
+   */
+  @Put("/{id}")
+  public async update(@Path() id: number, @Body() body: IChallengeUpdate): Promise<IUpdateResponse> {
+    return await Crud.Update<IChallengeUpdate>(body, TABLE_NAME, ['id'], [id]);
+  }
+}
