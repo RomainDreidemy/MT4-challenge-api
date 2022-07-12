b3e5cad9ea2883a99a14e1193d545b892c5e7641
Romain Dreidemy <45939424+RomainDreidemy@users.noreply.github.com>
2022-07-12 16:25:44 +0200

    add on cascade delete (#36)

--- database/ddl/ddl.sql
+++ database/ddl/ddl.sql
@@ -10,11 +10,6 @@ create table if not exists `batch` (
   `name` varchar(255) unique
 );
 
-create table if not exists `batch_challenge` (
-  `batch_id` int not null,
-  `challenge_id` int not null
-);
-
 create table if not exists `challenge` (
   `id` int primary key auto_increment,
   `name` varchar(255) unique,
@@ -31,10 +26,10 @@ create table if not exists `score` (
   `last_try_at` datetime not null
 );
 
-alter table `user` add foreign key (`batch_id`) references `batch` (`id`);
+alter table `user` add foreign key (`batch_id`) references `batch` (`id`) on cascade delete;
 
-alter table `batch_challenge` add foreign key (`batch_id`) references `batch` (`id`);
+alter table `batch_challenge` add foreign key (`batch_id`) references `batch` (`id`) on cascade delete;
 
-alter table `score` add foreign key (`user_id`) references `user` (`id`);
+alter table `score` add foreign key (`user_id`) references `user` (`id`) on cascade delete;
 
-alter table `score` add foreign key (`challenge_id`) references `challenge` (`id`);
+alter table `score` add foreign key (`challenge_id`) references `challenge` (`id`) on cascade delete;
