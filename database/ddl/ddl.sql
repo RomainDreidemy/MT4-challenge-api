a7adc2dd3568d0fa494913bc1f69327b8181dd66
Romain Dreidemy <45939424+RomainDreidemy@users.noreply.github.com>
2022-07-13 11:05:07 +0200

    set batch_id to null (#40)

--- database/ddl/ddl.sql
+++ database/ddl/ddl.sql
@@ -1,7 +1,7 @@
 create table if not exists `user` (
   `id` int primary key auto_increment,
   `email` varchar(255) unique,
-  `batch_id` int NOT NULL,
+  `batch_id` int,
   `is_admin` int not null default 0
 );
 
